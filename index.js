9b6824ec5f904a7f4a979410fa7c02be328a3184
Marc Jean-Gilles <marcjeangilles@gmail.com>
2018-03-28 04:23:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function whisper(string){
     )
 }
 function logShout(string){
+<<<<<<< HEAD
  console.log(string.toUpperCase())
 }
 function logWhisper(string){
@@ -25,4 +26,15 @@ function sayHiToGrandma(string){
   if ("I love you, Grandma." === string){
   return "I love you, too.";
 }
+=======
+  const spy = expect.spyOn(console, 'log').andCallThrough()
+  console.log()
+  return string.toUpperCase(
+    )
+
+    logShout(spy)
+
+
+    console.log.restore()
+>>>>>>> 8004cdeda1196d6ae3d563da88542056f938a612
 }
